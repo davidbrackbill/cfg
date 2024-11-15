29f0d97a5e95abf819f99c81108c4cca1f92f25d
David Brackbill <dmbrackbill@gmail.com>
2024-11-14 16:54:56 -0800

    disable clangd for protobuf files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -667,7 +667,7 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }}, -- exclude .proto
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
