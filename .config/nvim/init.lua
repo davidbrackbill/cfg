74e21885faf2d446334b9018eecfd15d87be4832
David Brackbill <dmbrackbill@gmail.com>
2024-12-16 17:00:15 -0800

    Have Mason auto-install my LSPs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -670,11 +670,10 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }}, -- exclude .proto
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  clangd = {filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }}, -- exclude .proto
+  gopls = {},
+  pyright = {},
+  svelte = {},
 
   lua_ls = {
     Lua = {
