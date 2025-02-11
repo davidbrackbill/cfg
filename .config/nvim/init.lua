44e49d6c8b67019d40103544f2eee75fdb408d84
David Brackbill <dmbrackbill@gmail.com>
2025-02-11 14:08:28 -0800

    Add autopairs, which-key->helix theme and remove unused suggestions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -119,16 +119,13 @@ require('lazy').setup({
   {
     'folke/which-key.nvim',
     opts = {
-      -- Key-chains
+      preset = "helix",
+      delay = 0,
       spec = {
         { '<leader>c', group = '[C]ode' },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>h', group = '[G]it' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>',  group = 'VISUAL <leader>', mode = { 'v' } },
         { '<leader>h', group = 'Git [H]unk',      mode = { 'v' } },
       }
     }
@@ -315,6 +312,14 @@ require('lazy').setup({
     },
   },
 
+{
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -335,7 +340,6 @@ require('lazy').setup({
 -- See `:help vim.keymap.set()`
 
 -- Enter command mode with ;
-vim.keymap.set("", ":", ";")
 vim.keymap.set("", ";", ":")
 
 -- Ignore leader
@@ -540,12 +544,6 @@ end, 0)
 -- [[ Configure LSPs ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible -- Up/down is swapped from normal vim keys because using "dropdown" theme
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
@@ -554,25 +552,18 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>n', vim.lsp.buf.rename, 're[n]ame')
+  nmap('\\!', vim.lsp.buf.code_action, 'Code action[!]')
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  -- nmap('<leader>wl', function()
-    -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  -- end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
