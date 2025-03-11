3563d0cdfea5b015fc4a36ef20b239d12074fea9
David Brackbill <dmbrackbill@gmail.com>
2025-03-11 10:34:55 -0700

    reformat

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -644,8 +644,8 @@ null_ls.setup({
 require('nvim-ts-autotag').setup({
   opts = {
     -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
+    enable_close = true,          -- Auto close tags
+    enable_rename = true,         -- Auto rename pairs of tags
     enable_close_on_slash = false -- Auto close on trailing </
   },
   -- Also override individual filetype configs, these take priority.
