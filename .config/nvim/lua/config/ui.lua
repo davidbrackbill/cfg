2b545c6ad99f14a9649f0939697b7eadcd8b9fe2
David Brackbill <dbrackbill@launchdarkly.com>
2026-02-26 14:25:43 -0800

    Add neovide config

--- .config/nvim/lua/config/ui.lua
+++ .config/nvim/lua/config/ui.lua
@@ -21,3 +21,9 @@ for _, group in ipairs(WKGroups) do
   -- FG: Onedark, BG: transparent
   vim.api.nvim_set_hl(0, group, { fg = "#abb2bf", bg = "NONE" })
 end
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_position_animation_length = 0
+end
