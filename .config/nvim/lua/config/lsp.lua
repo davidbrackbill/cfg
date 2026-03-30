db94da5f9caf423ae95a8d8b7f00e977dd9080b1
David Brackbill <dbrackbill@launchdarkly.com>
2026-03-30 13:58:52 -0700

    git: just autoformat on editor save

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -21,6 +21,13 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
 end
 
 -- mason-lspconfig requires mason to be set up first
