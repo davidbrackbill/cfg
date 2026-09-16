2aa33513a6378a17d4fa9f36734c5364bdcae27e
David Brackbill <dbrackbill@launchdarkly.com>
2026-09-16 10:17:21 -0700

    Delete AWS VPN auto-load (new update borked it)

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,11 +4,9 @@ require('hs.ipc')
 hs.window.animationDuration = 0
 
 local windows = require('windows')
-local network = require('network')
 local keys    = require('keys')
 local mouse   = require('mouse')
 
 keys.bind()
 windows.startWatcher()
-network.startWatcher()
 mouse.startWatcher()
